96b1212c864672ca24c6b517472a0730b08bfd6c
bubblepou <kapouwork@gmail.com>
2022-03-21 22:33:43 +0800

    first

---
+++ content.js
@@ -0,0 +1,80 @@
+let btn = document.querySelectorAll(".process_number");
+let number = 0;
+
+
+if (document.title == 'Miopane'){
+    var imgSrc = ['./image/Miopane/description/1.png', './image/Miopane/description/2.png', './image/Miopane/description/3.png'];
+} else if (document.title == '甜報'){
+    var imgSrc = ['./image/sweet/description-1.png','./image/sweet/description-2.png'];
+}
+
+btn.forEach((item, index) => {
+    item.addEventListener("click", change_image => {
+        document.querySelector('#process_image').src = imgSrc[index];
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].style.color = '#828282';
+        }
+        btn[index].style.color = '#F8F8F8';
+        let number = index;
+        console.log(number)
+    });
+});
+
+// hovering= #BDBDBD 原來＝#F8F8F8 暗＝#828282
+
+let goHome = document.querySelectorAll('.goHome');
+let leaveBox = document.querySelector('#leave');
+let next = document.querySelector('#next');
+
+goHome.forEach((element) => {
+    element.addEventListener('click', function leave() {
+        leaveBox.style.backgroundColor = "#FFF8F1";
+        leaveBox.style.animation = 'leave 0.5s forwards';
+        setTimeout(() => { document.location.href = element.getAttribute('data-href'); }, 500)
+    });
+})
+
+next.addEventListener('click', function leave() {
+    leaveBox.style.backgroundColor = next.getAttribute('data-color');;
+    leaveBox.style.animation = 'leave 0.5s forwards';
+    setTimeout(() => { document.location.href = next.getAttribute('data-href'); }, 500)
+});
+
+function enter() {
+    document.querySelector('#enter').style.animation = 'enter 0.8s forwards';
+}
+
+function goTop() {
+    document.querySelector('body').scrollIntoView({
+        behavior: 'smooth'
+    })
+}
+
+// menu click //
+
+let openMenu = document.querySelectorAll('.icons-img > img');
+let iconsImg = document.querySelectorAll('.icons-img');
+let menu = document.querySelectorAll('.icons-text');
+let closeBtn = document.querySelectorAll('.menuClose');
+
+openMenu.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        if (window.innerWidth < 500){
+            iconsImg[index].style.zIndex = '10';
+            document.querySelector('.go-top').style.zIndex = '-1';
+            menu[index].style.right = '-20px';
+        }
+    })
+})
+
+closeBtn.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        if (window.innerWidth < 500){
+            menu[index].style.right = 'calc( -20px - 100vw)';
+            setTimeout(()=>{
+                iconsImg[index].style.zIndex = '2';
+                document.querySelector('.go-top').style.zIndex = '1';
+            },300);
+        }
+    })
+})
---
+++ index.js
@@ -0,0 +1,150 @@
+// ! Circular text ! //
+
+// Math.PI / 180 = 1度
+const angleToRadian = (angle) =>{
+    return angle * (Math.PI / 180);
+};
+
+var radius = 280;
+var diameter = radius * 2;
+
+const circle = document.querySelector('#circle-text');
+circle.style.width = `${diameter}px`;
+circle.style.height = `${diameter}px`;
+
+const text = circle.innerText;
+const characters = text.split('');
+circle.innerText = null;
+
+let angle = -90;
+const deltAngle = 360 / characters.length;
+
+function makeCircle(radius){
+    characters.forEach((char,index) => {
+    const charElement = document.createElement('span');
+    charElement.innerText = char;
+
+    const xPos = radius * (1+Math.cos(angleToRadian(angle)));
+    const yPos = radius * (1+Math.sin(angleToRadian(angle)));
+
+    const transform = `translate(${xPos}px, ${yPos}px)`;
+    const rotate = `rotate(${index * deltAngle}deg)`;
+    charElement.style.transform = `${transform} ${rotate}`;
+    
+    angle += deltAngle;
+    circle.appendChild(charElement);
+    });
+}
+
+change_Circle_Size()
+
+// RWD //
+
+function change_Circle_Size(){
+    circle.innerText = null;
+    let windowWidth = window.innerWidth;
+    console.log('windowWidth',windowWidth)
+    if (windowWidth > 1100){
+        var radius = 280;
+    }else if (windowWidth <= 1100 && windowWidth > 970){
+        var radius = 250;
+    }else if (windowWidth <= 970 && windowWidth > 800){
+        var radius = 220;
+    }else if (windowWidth <= 970 && windowWidth > 620){
+        var radius = 180;
+    }else if (windowWidth <= 620){
+        var radius = 140;
+    };
+    var diameter = radius * 2;
+    circle.style.width = `${diameter}px`;
+    circle.style.height = `${diameter}px`;
+    makeCircle(radius);
+}
+
+window.addEventListener("resize", change_Circle_Size);
+
+
+// light box //
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "flex";
+}
+
+// enter and leave //
+
+let works = document.querySelectorAll('.work');
+let leaveBox = document.querySelector('#leave');
+
+works.forEach( (element) =>{
+    element.addEventListener('click', function leave(){
+        leaveBox.style.backgroundColor = element.getAttribute('data-color');
+        leaveBox.style.animation = 'leave 0.5s forwards';
+        setTimeout(()=>{document.location.href=element.getAttribute('data-href');},500)
+    });
+    element.addEventListener('mouseenter', ()=>{
+        document.querySelector('body').style.backgroundColor = element.getAttribute('data-color');
+    });
+    element.addEventListener('mouseleave', ()=>{
+        document.querySelector('body').style.backgroundColor = '#FFF8F1';
+    });
+})
+
+function enter(){
+    document.querySelector('#enter').style.animation = 'enter 0.8s forwards';
+}
+
+
+// navigation-scroll //
+
+let toItem = document.querySelectorAll('.scrollTo');
+let clickItem = document.querySelectorAll('.hover-line');
+
+clickItem.forEach(
+    (item,index) => {
+        item.addEventListener('click',()=>{
+            if (index > 0){
+                toItem[index-1].scrollIntoView({
+                    behavior: 'smooth'
+                })
+            }else{
+                toItem[0].scrollIntoView({
+                    behavior: 'smooth'
+                })
+            }
+        })
+    }
+)
+
+function goTop(){
+    document.querySelector('body').scrollIntoView({
+        behavior: 'smooth'
+    })
+}
